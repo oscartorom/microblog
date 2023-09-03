2769467fc5f2f89db81f60d480c0e9192d604514
Oscar Toro <oscartoromalaga@outlook.com>
2023-09-03 19:57:00 +1000

    dev: added for loop to show posts

--- app/routes.py
+++ app/routes.py
@@ -5,4 +5,16 @@ from app import app
 @app.route('/index')
 def index():
     user = {'username': 'Oscar'}    
-    return render_template('index.html', title='home', user=user)
+    posts = [
+        {
+            "author": {"username": "John"},
+            "body": "Beautiful day in Sydney!"
+        },
+        {
+            "author": {"username": "Susan"},
+            "body": "I hate movies"
+        }
+    ]
+    
+    return render_template('index.html', title='home', user=user, posts=posts)
+
