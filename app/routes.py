0a7eb2a15633ed576dc3b18a0867434f834f654b
Oscar Toro <oscartoromalaga@outlook.com>
2023-09-13 21:50:44 +1000

    Added route to user

--- app/routes.py
+++ app/routes.py
@@ -71,3 +71,14 @@ def register():
         return redirect(url_for('login'))
     
     return render_template('register.html', title='Register', form=form)
+
+## Flask allows for dynamic inputs: <>... this can be used later within the function
+@app.route('/user/<username>')
+@login_required
+def user(username):
+    user = User.query.filter_by(username=username).first_or_404()
+    posts = [
+        {'author': user, 'body': 'Test post #1'},
+        {'author': user, 'body': 'Test post #2'}
+    ]
+    return render_template('user.html', user=user, posts=posts)
