681f873c92741345c323d70c396a68e9306d4745
Oscar Toro <oscartoromalaga@outlook.com>
2023-09-14 21:08:33 +1000

    dev: created a method to send logs to a file

--- app/__init__.py
+++ app/__init__.py
@@ -6,7 +6,8 @@ from flask_migrate import Migrate
 from flask_login import LoginManager
 
 import logging
-from logging.handlers import SMTPHandler
+from logging.handlers import SMTPHandler, RotatingFileHandler
+import os
 
 app = Flask(__name__)
 
@@ -22,6 +23,8 @@ login = LoginManager(app)
 login.login_view = 'login'
 
 if not app.debug:
+    
+    # Mail handler
     if app.config['MAIL_SERVER']:
         auth = None
         if app.config['MAIL_USERNAME'] or app.config['MAIL_PASSWORD']:
@@ -39,5 +42,14 @@ if not app.debug:
         mail_handler.setLevel(logging.ERROR)
         app.logger.addHandler(mail_handler)
 
+    # File handler
+    if not os.path.exists('logs'):
+        os.mkdir('logs')
+    file_handler = RotatingFileHandler('logs/microblog.log', maxBytes=10240, backupCount=10)
+    file_handler.setFormatter(logging.Formatter('%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]'))
+    file_handler.setLevel('logging.INFO')
+    app.logger.addHandler(file_handler)
+
+    app.logger.info('Microblog startup')
 
 from app import routes, models, errors
