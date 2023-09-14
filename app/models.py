645a4f75794a22736cf6f58db33614a002dc5091
Oscar Toro <oscartoromalaga@outlook.com>
2023-09-14 21:46:29 +1000

    dev: created followers relationships within users, and migrated db

--- app/models.py
+++ app/models.py
@@ -7,14 +7,31 @@ from hashlib import md5
 from werkzeug.security import generate_password_hash, check_password_hash
 from datetime import datetime
 
+# Not declaring as a model, since this is an auxiliary table to only hold foreign keys.
+followers = db.Table(
+    'followers',
+    db.Column('follower_id', db.Integer, db.ForeignKey('user.id')),
+    db.Column('followed_id', db.Integer, db.ForeignKey('user.id'))
+    )
+
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
     password_hash = db.Column(db.String(128))
-    posts = db.relationship('Post', backref='author', lazy='dynamic')
+    
     about_me = db.Column(db.String(140))
     last_seen = db.Column(db.DateTime, default=datetime.utcnow)
+
+    posts = db.relationship('Post', backref='author', lazy='dynamic')
+    
+    followed = db.relationship(
+        'User',
+        secondary=followers,
+        primaryjoin=(followers.c.follower_id == id), 
+        secondaryjoin=(followers.c.followed_id == id), 
+        backref=db.backref('followers', lazy='dynamic'),
+        lazy='dynamic')
 
     def set_password(self, password):
         self.password_hash = generate_password_hash(password)
@@ -42,9 +59,3 @@ class Post(db.Model):
 def load_user(id):
     return User.query.get(int(id))
 
-# Not declaring as a model, since this is an auxiliary table to only hold foreign keys.
-followers = db.Table(
-    'followers',
-    db.Column('follower_id', db.Integer, db.ForeignKey('user.id')),
-    db.Column('followed_id', db.Integer, db.ForeignKey('user.id'))
-    )
