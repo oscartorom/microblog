699c594de0ca9a8aed75bee89a14ead45ed5e1f2
Oscar Toro <oscartoromalaga@outlook.com>
2023-09-15 21:39:04 +1000

    dev: created method to add/create posts

--- app/routes.py
+++ app/routes.py
@@ -5,15 +5,22 @@ from werkzeug.urls import url_parse
 from datetime import datetime
 
 from app import app, db
-from app.forms import LoginForm, RegistrationForm, EditProfileForm, EmptyForm
-from app.models import User
+from app.forms import LoginForm, RegistrationForm, EditProfileForm, EmptyForm, PostForm
+from app.models import User, Post
 
 
 @app.route('/')
 @app.route('/index')
 @login_required
 def index():
-    
+    form = PostForm()
+    if form.validate_on_submit():
+        post = Post(body=form.post.data, author=current_user)
+        db.session.add(post)
+        db.session.commit()
+        flash('Your post is now live!')
+        return redirect(url_for('index'))
+
     posts = [
         {
             "author": {"username": "John"},
@@ -25,7 +32,7 @@ def index():
         }
     ]
     
-    return render_template('index.html', title='home', posts=posts)
+    return render_template('index.html', title='home', posts=posts, form=form)
 
 @app.route('/login', methods=['GET','POST'])
 def login():
