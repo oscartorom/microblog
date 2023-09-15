c2ef849e3939082a645a41f1e40baf6c70d3f5fa
Oscar Toro <oscartoromalaga@outlook.com>
2023-09-15 22:14:46 +1000

    dev: added functionality to paginate and navigate posts

--- app/routes.py
+++ app/routes.py
@@ -9,8 +9,8 @@ from app.forms import LoginForm, RegistrationForm, EditProfileForm, EmptyForm, P
 from app.models import User, Post
 
 
-@app.route('/')
-@app.route('/index')
+@app.route('/', methods=['GET', 'POST'])
+@app.route('/index', methods=['GET','POST'])
 @login_required
 def index():
     form = PostForm()
@@ -21,9 +21,12 @@ def index():
         flash('Your post is now live!')
         return redirect(url_for('index'))
 
-    posts = current_user.followed_posts().all()
+    page = request.args.get('page', 1, type=int)
+    posts = current_user.followed_posts().paginate(page=page, per_page=app.config['POSTS_PER_PAGE'], error_out=False)
+    next_url = url_for('index', page=posts.next_num) if posts.has_next else None
+    prev_url = url_for('index', page=posts.prev_num) if posts.has_prev else None
     
-    return render_template('index.html', title='home', posts=posts, form=form)
+    return render_template('index.html', title='home', posts=posts.items, form=form, next_url=next_url, prev_url=prev_url)
 
 @app.route('/login', methods=['GET','POST'])
 def login():
@@ -143,3 +146,11 @@ def unfollow(username):
     else:
         return redirect(url_for('index'))
 
+@app.route('/explore')
+@login_required
+def explore():
+    page = request.args.get('page', 1, type=int)
+    posts = Post.query.order_by(Post.timestamp.desc()).paginate(page=page, per_page=app.config['POSTS_PER_PAGE'], error_out=False)
+    next_url = url_for('explore', page=posts.next_num) if posts.has_next else None
+    prev_url = url_for('explore', page=posts.prev_num) if posts.has_prev else None
+    return render_template('index.html', title='Explore', posts=posts.items, next_url=next_url, prev_url=prev_url)
--- config.py
+++ config.py
@@ -13,3 +13,5 @@ class Config():
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
     ADMINS = ['oscartoromalaga@outlook.com']
+
+    POSTS_PER_PAGE = 3
