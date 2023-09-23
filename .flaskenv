1c961cad1daea8fdf5772fd75fa94f3888908b89
Oscar Toro <oscartoromalaga@outlook.com>
2023-09-23 21:15:14 +1000

    dev: changed gitignore to ignore env files

--- .flaskenv
+++
@@ -1,4 +0,0 @@
-FLASK_APP=microblog.py
-FLASK_ENV=development
-FLASK_DEBUG=1
-TEMPLATES_AUTO_RELOAD=1
--- config.py
+++ config.py
@@ -12,6 +12,6 @@ class Config():
     MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') is not None
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
-    ADMINS = ['oscartoromalaga@outlook.com']
+    ADMINS = ['oscartoromal@gmail.com']
 
     POSTS_PER_PAGE = 25
