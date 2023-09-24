a1deaa88a35eac092291798733c9f175a56efb2a
Oscar Toro <oscartoromalaga@outlook.com>
2023-09-24 11:34:46 +1000

    dev: bootstraped the page

--- app/__init__.py
+++ app/__init__.py
@@ -5,6 +5,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
 from flask_mail import Mail
+from flask_bootstrap import Bootstrap
 
 import logging
 from logging.handlers import SMTPHandler, RotatingFileHandler
@@ -25,6 +26,8 @@ login.login_view = 'login'
 
 # Sending e-mails!
 mail = Mail(app)
+
+bootstrap = Bootstrap(app)
 
 if not app.debug:
     
